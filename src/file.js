bed1880690f7bfbfef0ee377f75dd3c462ce4ea4
Nicolas Thouvenin <nthouvenin@gmail.com>
2019-01-14 13:47:11 +0100

    imporve File function

--- src/file.js
+++ src/file.js
@@ -1,8 +1,11 @@
-import { readFileSync } from 'fs';
+import { readFileSync, statSync } from 'fs';
 import { dirname } from 'path';
 
 export default function File(ezs, filename) {
     try {
+        if (!statSync(filename).isFile()) {
+            return null;
+        }
         ezs.addPath(dirname(filename));
         return readFileSync(filename, 'utf8');
     } catch (e) {
