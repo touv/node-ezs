55eb3567cdedfd798b7c4723b228b3708f411ee7
Nicolas Thouvenin <nthouvenin@gmail.com>
2019-02-26 22:03:23 +0100

    finalize

--- src/settings.js
+++ src/settings.js
@@ -2,20 +2,20 @@ import os from 'os';
 
 const settings = {
     highWaterMark: {
-        object: 16,
-        bytes: 16384,
+        object: Number(process.env.EZS_HIGHWATERMARK_OBJECT || 16),
+        bytes: Number(process.env.EZS_HIGHWATERMARK_BYTES || 16384),
     },
-    nShards: os.cpus().length,
-    encoding: 'gzip',
+    nShards: Number(process.env.EZS_NSHARDS || os.cpus().length),
+    encoding: String(process.env.EZS_ENCODING || 'gzip'),
     servePath: process.cwd(),
     cache: {
-        root: os.tmpdir(),
-        dir: '/ezs',
-        files: 100,
-        size: '1 GB',
-        check: 10,
+        root: String(process.env.EZS_CACHE_ROOT || os.tmpdir()),
+        dir: String(process.env.EZS_CACHE_DIR || '/ezs'),
+        files: Number(process.env.EZS_CACHE_FILES || 100),
+        size: String(process.env.EZS_CACHE_SIZE || '1 GB'),
+        check: Number(process.env.EZS_CACHE_CHECK || 10),
     },
-    port: 31976,
+    port: Number(process.env.EZS_PORT || 31976),
 };
 
 export default settings;
